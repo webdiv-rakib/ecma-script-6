189ce05b3ca1dcba8f0509badd227dab3e08487a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-12-10 00:24:59 +0600

    when to use var/let/const

--- var-let-const.js
+++ var-let-const.js
@@ -10,5 +10,23 @@ count = count + 10;
 console.log(count);
 
 const numbers = [24, 4, 7, 12, 1];
-numbers = [4, 5, 6, 7];
+// numbers = [4, 5, 6, 7];
+numbers[1] = 55;
+numbers.push(4, 2, 1);
 console.log(numbers);
+
+// Object
+const student = {
+    name: 'moyna pakhi',
+    class: 12,
+}
+// student = { name: 'kokil' };
+student.name = 'kokil';
+console.log(student);
+
+// loop
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(sum);
