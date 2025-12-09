3010871d6fd4447762a4bfebc39bcd8fc34a5e17
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-12-10 01:10:52 +0600

    string template/back tick

---
+++ template.js
@@ -0,0 +1,11 @@
+const first = 'jaan';
+const last = 'pakhi';
+const name = first + ' ' + last;
+console.log(name);
+const a = 10;
+const b = 20;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a + b}`
+console.log(math);
