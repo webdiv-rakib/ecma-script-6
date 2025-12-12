61e97fc92d60734a3d5a703549c978a7d6c79cbd
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-12-12 17:04:46 +0600

    how to declare arrow function.

---
+++ arrow.js
@@ -0,0 +1,21 @@
+// function add(a, b) {
+//     const result = a + b;
+//     return result;
+// }
+
+function add(a, b) {
+    return a + b;
+}
+
+// function expression
+const add2 = function (a, b) {
+    return a + b;
+}
+
+// arrow function
+const add3 = (a, b) => a + b;
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+// const sum = add(5, 5);
+const sum = add4(4, 5, 6, 7);
+console.log(sum);
--- es6-recap/arrow.js
+++
@@ -1,7 +0,0 @@
-function add(a, b) {
-    const result = a + b;
-    return result;
-}
-
-const sum = add(2, 5);
-console.log(sum);
