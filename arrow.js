bbcf19f00951cb2b56796896bbc46136088b1674
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2025-12-24 22:15:21 +0600

    bit update on my exam

--- arrow.js
+++ arrow.js
@@ -20,4 +20,5 @@ const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
 const sum = add4(4, 5, 6, 7);
 console.log(sum);
 
-// tomorrow is my Art of living exam and also last exam
+// tomorrow is my Art of living exam and also last exam'
+// final examination just finished today
