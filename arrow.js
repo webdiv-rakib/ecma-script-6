5c65926c72fef189998c2bdb72b6259ea16b1e46
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-02 22:09:31 +0600

    removed all the comments that we dont need

--- arrow.js
+++ arrow.js
@@ -19,16 +19,3 @@ const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
 // const sum = add(5, 5);
 const sum = add4(4, 5, 6, 7);
 console.log(sum);
-
-// tomorrow is my Art of living exam and also last exam'
-// final examination just finished today
-// another day wasted successfully
-// today i bought my new winter jacket
-// going for the shopping
-// today i am going home now
-// i am at my home now cant work
-// will start working 
-// this year last commit
-// end of 2025sss
-// new year first day no work
-// lets start working
