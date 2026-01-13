d7742761688445e2e3c38afeb7e2d63d48fb3c2f
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-14 02:14:56 +0600

    arrow function,multiple para,function body.

--- arrow.js
+++ arrow.js
@@ -15,18 +15,10 @@ const add2 = function (a, b) {
 // arrow function
 const add3 = (a, b) => a + b;
 const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+const multiply = (num1, num2) => num1 * num2;
 
 // const sum = add(5, 5);
-const sum = add4(4, 5, 6, 7);
-console.log(sum);
-// lets start working from tomorrow
-// ki likhmu kisui jani na
-// busy with my final defense
-// just added a comment
-// successfully wasted 7days
-// mama is dead pay for him
-// today i just bought sneakers
-// lets play some bathminton
-// just applied for convocation
-// lets get into work
-// tomorrow i will submit my final defense work
+// const sum = add4(4, 5, 6, 7);
+// console.log(sum);
+const multi = multiply(2, 1);
+console.log(multi);
