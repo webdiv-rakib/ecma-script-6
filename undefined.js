652147a87d19beb383360a1c14272dc0dc7b7e9e
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-19 04:16:57 +0600

    cause that gives undefined.

---
+++ undefined.js
@@ -0,0 +1,12 @@
+/**
+ * 8 Ways to get undefine
+ * 
+ * 1.variable that is not initialized will give undefined.
+ * 2.function with no return.
+ * 3.parameter that is not passed will be undefined.
+ * 4.if return has nothing on the right side will return undefined. 
+ * 5.property that does not exits on an object will give you undefined.
+ * 6.accessing array elements outside off the index rage will give undefined.
+ * 7.deleting an element inside an array.
+ * 8.set a value directly to undefined.
+ */
