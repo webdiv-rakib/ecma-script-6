8daeb86658baee96d001fa9458ccad2eacda663b
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-19 04:01:28 +0600

    js dynamic type language also data type

---
+++ types.js
@@ -0,0 +1,24 @@
+/**
+ * JS => dynamic type programming language
+ */
+
+// primitive
+const a = 5;
+const b = 'abul';
+const d = true;
+
+// non-primitive
+const ages = [11, 22, 33];
+const student = {
+    id: 21,
+    class: 7
+}
+
+
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
