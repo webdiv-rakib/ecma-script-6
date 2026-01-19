f4e30ca97aa7d49f35d3a690554700ab8003e44a
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-20 02:40:40 +0600

    scope example.

---
+++ scope.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    const total = a + b;
+    if (b > 5) {
+        const sum = 25 + a + b;
+    }
+    else {
+        const sum = 5 + a + b;
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+}
+// const sum = add(5, 3);
+// console.log(sum);
+add(5, 3);
