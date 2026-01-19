a59d5a8502c141cb2d057348f6847accc640eea1
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-20 02:25:34 +0600

    double equal and triple equal use case.

---
+++ triple.js
@@ -0,0 +1,13 @@
+const first = 1;
+const second = true;
+if (first == second) {
+    console.log('values are equal');
+}
+else {
+    console.log('values are not equal');
+}
+/**
+ * type coercion
+ * type conversion
+ * type casting
+ */
