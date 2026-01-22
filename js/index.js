f2885f0c14b6103cf8fd01b6fcb56bedbbc8b1b9
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-23 02:16:30 +0600

    dynamically display data on website.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.log('index.js connected');
+function loadUsers() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadUsersData(data))
+}
+
+function loadUsersData(data) {
+    const ul = document.getElementById('users-list');
+    for (const user of data) {
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+}
