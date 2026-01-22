8bd3f015a87595a27244d646b520338f63923145
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-23 04:00:51 +0600

    practice on loading user comments.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // =====Posts=====
 function loadPosts() {
     const url = 'https://jsonplaceholder.typicode.com/posts';
@@ -7,12 +6,27 @@ function loadPosts() {
         .then(data => displayPosts(data))
 }
 function displayPosts(data) {
-    console.log(data);
     const ul = document.getElementById('posts-list');
     for (const post of data) {
         console.log(post);
         const li = document.createElement('li');
         li.innerText = post.title;
+        ul.appendChild(li);
+    }
+}
+
+// =====Comments=====
+function loadComments() {
+    const url = 'https://jsonplaceholder.typicode.com/comments'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayComments(data));
+}
+function displayComments(data) {
+    const ul = document.getElementById('comments-list');
+    for (const comment of data) {
+        const li = document.createElement('li');
+        li.innerText = comment.email;
         ul.appendChild(li);
     }
 }
@@ -28,7 +42,6 @@ function loadUsers() {
 function loadUsersData(data) {
     const ul = document.getElementById('users-list');
     for (const user of data) {
-        console.log(user.name);
         const li = document.createElement('li');
         li.innerText = user.name;
         ul.appendChild(li);
