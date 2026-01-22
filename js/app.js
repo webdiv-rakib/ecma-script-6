ecbe5652bbf2dc35e6d3c1627b71beff13c51d74
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-23 01:41:39 +0600

    task about more apis.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
+function loadPosts() {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
 
+// ==============================
+function loadComments() {
+    const url = 'https://jsonplaceholder.typicode.com/comments';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayComments(data))
+}
 
+function displayComments(data) {
+    console.log(data);
+};
+// ==============================
+
+function loadAlbums() {
+    const url = 'https://jsonplaceholder.typicode.com/albums';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAlbums(data))
+}
+
+function displayAlbums(data) {
+    console.log(data);
+}
