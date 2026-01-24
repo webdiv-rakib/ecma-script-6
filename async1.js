03f354fbae229ea13df98ee7c3c44a133ae141e7
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-01-24 18:21:02 +0600

    async and await structure.

---
+++ async1.js
@@ -0,0 +1,27 @@
+const myLoader = () => {
+    return new Promise((resolve, reject) => {
+        const success = Math.random();
+        if (success <= 0.5) {
+            resolve(success);
+        }
+        else {
+            reject(success);
+        }
+    })
+}
+myLoader()
+    .then(data => console.log('resolved data', data))
+    .catch(err => console.log('rejected with value', err))
+
+// ====================================
+fetch('')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+async function loadData() {
+    const res = await fetch('');
+    const data = await res.json();
+    console.log(data);
+}
+loadData();
