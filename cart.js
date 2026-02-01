36abf544b257fcbbe5b4b9b3278830efbc75dd47
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-01 23:22:58 +0600

    Local Storage Interactions for a shopping Cart

--- cart.js
+++ cart.js
@@ -7,10 +7,40 @@ const addProduct = () => {
     quantityField.value = '';
     console.log(product, quantity);
     displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
+
 const displayProduct = (product, quantity) => {
     const ul = document.getElementById('product-container')
     const li = document.createElement('li');
     li.innerText = `${product}:${quantity}`
     ul.appendChild(li);
 }
+
+const getStoredShoppingCart = () => {
+    const storedCart = localStorage.getItem('cart');
+    let cart = {};
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for (const product in savedCart) {
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
