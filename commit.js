ad817f05f27d16f096d2e981dba8da40a7627a09
Abu Nayim Mohammad Rakib <mdrakibulhassan24@gmail.com>
2026-02-14 20:45:11 +0600

    Update commit.js

--- commit.js
+++ commit.js
@@ -4,23 +4,4 @@ console.log('just deleted an unnecessary file that i dont really need ');
 console.log('just tried github mobile to commit');
 console.log('hi');
 console.log('hi');
-//1. Just came home today for election.
-//2. Will get back hall very soon
-//3. Just builded first cv.
-//4. 10th February gonna meet my girl
-//5. i have to wake up at morning 
-//6. this file is like my personal dairy 
-//7. my this github mobile is so much handy
-//8. feels like eid day its 11/02/2026
-//9. tomorrow is national election day
-//10. don't know who will won but hope its Nasir uddin patwari
-//11. vote for dari palla i know they all are not good but lets see are they same as awamili and bnp or not
-//12. one more commit and they we are good to go
-// today 12/02/2026
-// hasnat abdullah first winner 
-// waiting for nasiruddin patwary 
-// paraying for him hope he will win
-// so bnp won 
-// jamat ncp lost
-// patuwary also lost
-// election commission recounting dhaka-8
+// 1. deleted old comments 
